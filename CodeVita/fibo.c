b66a243f29432a4a848640d59a65271561e6fee4
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-09-02 20:09:36 +0530

    modified fibonacci

---
+++ CodeVita/fibo.c
@@ -0,0 +1,57 @@
+/** Modified fibonacci problem of CodeVita 2018
+ * Input n and k
+ * Input first k fibonacci terms
+ * nth Fibonacci term = (product of previous k fibonacci terms nth time) MOD 10^9+7
+ * Output nth fibonacci number
+ * 1 <= n <= 10^6
+ * 1 <= k <= 10
+ * This solution takes O(n) time
+ */
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#define MOD 1000000007
+
+int main(int argc, char const *argv[])
+{
+	int t;
+	scanf("%d", &t);
+
+	int i;
+	for (i = 0; i < t; i++)
+	{
+		long long int n, k;
+		scanf("%lld %lld", &n, &k);
+		
+		int j;
+		long long int inp[n];
+		for (j = 0; j < k; j++)
+		{
+			scanf("%lld", &inp[j]);
+			// printf("%lld", inp[j]);
+		}
+
+		int pos = 0;
+		long long int mul = 1;
+		int knew = k;
+		for (j = 1; j < n-knew+1; j++)
+		{
+			int y;
+			for (y = pos; y < k; y++)
+			{
+				mul = mul * inp[y];
+				if (mul >= MOD)
+					mul %= MOD;
+			}
+			inp[k] = mul;
+			// printf("Next value: %lld\n", mul);
+			mul = 1;
+			k++;
+			pos++;
+		}
+		printf("%lld\n", inp[n-1]);
+	}
+
+	return 0;
+}
