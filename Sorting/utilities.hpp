06aba29ba15620ceae7297aecdadb9f1343156f1
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-09-06 14:24:56 +0530

    Header file that contains utility functions for sorting

---
+++ Sorting/utilities.hpp
@@ -0,0 +1,12 @@
+/** Contains utility functions for sorting programs */
+
+#include <bits/stdc++.h>
+using namespace std;
+
+void printV(vector <int> v)
+{
+	/** Print the elements of an int vector, separate them using spaces, and append a newline at the end of it */
+	for (int i: v)
+		cout << i << " ";
+	cout << endl;
+}
