f724012249612f7f6b228fce302f54ed96134248
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-09-08 17:50:40 +0530

    added function to print array

--- Sorting/utilities.hpp
+++ Sorting/utilities.hpp
@@ -10,3 +10,12 @@ void printV(vector <int> v)
 		cout << i << " ";
 	cout << endl;
 }
+
+void printA(int arr[], int size)
+{
+	/** Print the elements of an int array[size], separate them using spaces, and append a newline at the end of it */
+	// vector <int> v (begin(arr), end(arr));
+	for (int i = 0; i < size; i++)
+		cout << arr[i] << " ";
+	cout << endl;
+}
