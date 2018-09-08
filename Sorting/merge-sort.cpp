46f8e3d86917f47d0eeed9c3ec6ffd34e4887457
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-09-08 15:29:52 +0530

    got merge sort to working

--- Sorting/merge-sort.cpp
+++ Sorting/merge-sort.cpp
@@ -1,95 +1,78 @@
 /** MERGE SORT
- * Input: an unsorted vector of ints
- * Output: the same vector sorted through merge sort
+ * Input: an unsorted array of ints
+ * Output: the same array sorted through merge sort
  * Time: O(n lg n)
- * NOTE: The program doesn't work yet. 
- * TODO: Make the program work.
  */
 
 #include "utilities.hpp"
-#include <climits>
 
-vector <int> merge(vector <int>, int, int, int);
-vector <int> mergeSort(vector <int>, int begin, int end);
+void mergeSort(int arr[], int begin, int end);
+void merge(int arr[], int begin, int mid, int end);
 
-int main(void)
+int main()
 {
-	// TEST for MERGE function
-	// vector <int> v1 = {1,3,5,-1,2,4,6};
-	// vector <int> v2 = {1,3,4,5,8};
-	// vector <int> merged = merge(v1,0,2,6);
-	// printV(merged);
-
 	int n;
-	cout << "Enter number of elements: ";
+	cout << "Enter size of array: ";
 	cin >> n;
-	vector <int> v(n);
+	int arr[n];
 
-	cout << "Enter " << n << " elements:\n";
+	cout << "Enter " << n << " elements: ";
 	for (int i = 0; i < n; i++)
-		cin >> v[i];
+		cin >> arr[i];
 	
-	int begin, end;
-	begin = 0;
-	end = n-1;
-	v = mergeSort(v, begin, end);
-
-	cout << "Sorted elements:\n";
-	printV(v);
+	mergeSort(arr, 0, n-1);
+	cout << "Sorted array: ";
+	printA(arr, n);
 
 	return 0;
 }
 
-vector <int> merge(vector <int> v, int begin, int mid, int end)
+void mergeSort(int arr[], int begin, int end)
 {
-	/** v is a vector such that the subvectors v[begin:mid] and v[mid+1:end] are sorted
-	 * combine these subvectors to result a sorted vector
-	 */
+	/** Apply the merge sort algorithm to the array arr */
 
-	vector <int> left, right;
-
-	// fill in data into left subvector
-	for (int i = begin; i <= mid; i++)
-		left.push_back(v[i]);
-	
-	for (int i = mid+1; i <= end; i++)
-		right.push_back(v[i]);
-
-	// cout << "Left sub: ";
-	// printV(left);
-	// cout << "Right sub: ";
-	// printV(right);
-	
-	// Append sentinels to each of the vectors
-	left.push_back(INT_MAX);
-	right.push_back(INT_MAX);
-
-	// Track positions of v1 and v2
-	int pos1, pos2;
-	pos1 = pos2 = 0;
-
-	// Merging happens here
-	for (int i = 0; i < v.size(); i++)
-	{
-		if (left[pos1] < right[pos2])
-			v[i] = left[pos1++];
-		else
-			v[i] = right[pos2++];
-	}
-
-	return v;
-}
-
-vector <int> mergeSort(vector <int> v, int begin, int end)
-{
 	if (begin < end)
 	{
 		int mid = (begin + end) / 2;
-		vector <int> firstHalf = mergeSort(v, begin, mid);
-		vector <int> secondHalf = mergeSort(v, mid+1, end);
-		v = merge(v, begin, mid, end);
+		mergeSort(arr, begin, mid);
+		mergeSort(arr, mid+1, end);
+		merge(arr, begin, mid, end);
 	}
-	
-	return v;	
 }
 
+void merge(int arr[], int begin, int mid, int end)
+{
+	/** arr is an array such that subarray arr[0:mid] and arr[mid+1:end] are sorted
+	 * Merge them in a way that arr becomes sorted
+	 */
+
+	// create left and right subarrays
+	int sizeL = mid-begin+1;
+	int sizeR = end-mid;
+	int left[sizeL];
+	int right[sizeR];
+
+	// add elements to left and right
+	for (int i = 0; i < sizeL; i++)
+		left[i] = arr[begin + i];
+	for (int i = 0; i < sizeR; i++)
+		right[i] = arr[mid+i+1];
+
+	int l = 0; // base index of left subarray
+	int r = 0; // base index of right subarray
+	int k = begin; // base index of merged array
+
+	// the merge process
+	while (l < sizeL && r < sizeR)
+	{
+		if (left[l] < right[r])
+			arr[k++] = left[l++];
+		else
+			arr[k++] = right[r++];
+	}
+
+	// append remaining elements
+	while (l < sizeL)	arr[k++] = left[l++];
+	while (r < sizeR)	arr[k++] = right[r++];
+
+}
