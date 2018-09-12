2a3f7f769e2c53f531b66aa055896488679837e5
Md Rafi Akhtar <alimdrafi@gmail.com>
2018-09-12 10:41:39 +0530

    modified the Catalan numbers program to compute the first n Catalan numbers (instead of nth Cataln number)

--- DP/catalan.cpp
+++ DP/catalan.cpp
@@ -1,4 +1,4 @@
-/** Find the nth Catalan number.
+/** Find the first n Catalan numbers.
  * A Catalan number satifies the following two properties:
  * C(0) = C(1) = 1; C(n) = sum(C(i).C(n-i-1)), from i = 0 to n-1
  */
@@ -6,7 +6,10 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int *cat;
 
+// DON'T CALL THIS FUNCTION
+// It correctly computes Catalan numbers, but uses brute-force divide and conquer in doing so, and ends up taking a lot of time for larger numbers
 unsigned long int catalan(int n)
 {
 	if (n <= 1) return 1;
@@ -17,9 +20,11 @@ unsigned long int catalan(int n)
 	return cat;
 }
 
+// CALL THIS FUNCTION
+// It computes the first n Catalan numbers in shorter span of time using tabulation
 unsigned long int catalan_dp(int n)
 {
-	int cat[n];
+	// int cat[n];
 	cat[0] = cat[1] = 1;
 
 	for (int i = 2; i <= n; i++)
@@ -38,10 +43,14 @@ int main(int argc, char *argv[])
 	cout << "Enter n: ";
 	cin >> n;
 
+	cat = new int[n+1];
+
 	// int cat[n];
 	// cat[0] = 1;
 	
-	cout << "Catalan number " << n << " = " << catalan_dp(n) << endl;
+	cout << "Catalan numbers from 0 to " << n << " are:\n";
+	for (int i = 0; i <= n; i++)
+		cout << "catalan (" << i << ") = " << catalan_dp(i) << endl;
 
 	return 0;
 }
